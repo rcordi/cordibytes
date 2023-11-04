2a1373b178d6f8ab83d91d4b9cf6182a741f98c0
rcordi <64288591+rcordi@users.noreply.github.com>
2023-11-04 17:35:04 -0400

    Initialize project using Create React App and first changes

---
+++ src/apps/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div id="content"> */}
+        {/* <About></About>
+        <Experience></Experience>
+        <Projects></Projects>
+        <Credits></Credits> */}
+    {/* </div> */}
+    {/* <SidebarNav /> */}
+    </div>
+    );
+}
+
+export default App;
